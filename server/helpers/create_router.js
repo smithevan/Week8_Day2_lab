335ae7fadecfeef746a171440afb1869a2849f35
Evan Smith <smith.m.evan@gmail.com>
2020-01-21 15:41:40 +0000

    create working

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -29,6 +29,17 @@ const createRouter = function (collection) {
       });
   });
 
+  router.post('/', (req, res) => {
+    const newData = req.body
+    collection.insertOne(newData)
+      .then(result => res.json(result.ops[0]))
+      .catch((err) => {
+        console.error(err);
+        res.status(500)
+        res.json({ status: 500, error: err })
+      })
+  })
+
   return router;
 };
 
